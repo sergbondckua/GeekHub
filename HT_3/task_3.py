a077571104ea34ed145c94b65516a229b43ce490
sergbondckua <sergninja@gmail.com>
2022-10-12 11:31:27 +0300

    Option for older versions of python

--- HT_3/task_3.py
+++ HT_3/task_3.py
@@ -4,4 +4,8 @@ dict_1 = {'foo': 'bar', 'bar': 'buz'}
 dict_2 = {'dou': 'jones', 'USD': 36}
 dict_3 = {'AUD': 19.2, 'name': 'Tom'}
 
-print(dict_1 | dict_2 | dict_3)
+try:
+    print(dict_1 | dict_2 | dict_3)
+except TypeError:
+    # Older versions of Python
+    print({**dict_1, **dict_2, **dict_3})
