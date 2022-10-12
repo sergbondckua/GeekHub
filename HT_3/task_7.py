0a511d83e778f54bcb8f446566674e52e16e86e0
sergbondckua <sergninja@gmail.com>
2022-10-12 09:04:30 +0300

    Added 1 more option

--- HT_3/task_7.py
+++ HT_3/task_7.py
@@ -2,6 +2,10 @@
 де ключ — <число>, а значення — <число>*<число>
 напр. 3 --> {0: 0, 1: 1, 2: 4, 3: 9}"""
 
-dict_generate = {i: i**2 for i in range(int(input('Enter a number: ')) + 1)}
+*number, = range(int(input('Enter a number: ')) + 1)
 
-print(dict_generate)
+# Using cycles
+print({i: i ** 2 for i in number})
+
+# Not using cycles
+print(dict(zip(number, map(lambda x: x ** 2, number))))
