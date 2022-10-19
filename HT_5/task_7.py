1f34cc2a9f1171007456ce2feb046f0b96591db9
sergbondckua <sergninja@gmail.com>
2022-10-19 08:55:34 +0300

    fixed update

--- HT_5/task_7.py
+++ HT_5/task_7.py
@@ -8,19 +8,30 @@
 from collections import Counter
 
 
+# Solution #1 without import module
 def count_element(lst):
-    """ Count same elements"""
+    """ Count element in the sequence"""
 
     lst_str = list(map(str, lst))
+    solution = [(item, lst_str.count(item),) for item in set(lst_str)]
+    print('#1 Solution:', ', '.join(f"{item[0]} -> {item[1]}" for item in solution))
 
-    solution = [(item, lst_str.count(item),) for item in set(lst_str) if lst_str.count(item) > 1]
-    print(', '.join(f"{item[0]} -> {item[1]}" for item in solution))
+    return solution
 
-    solution2 = dict(Counter(lst_str))
-    print(', '.join(f"{val} -> {count}" for val, count in solution2.items()))
 
-    return solution2  # solution
+# Solution #2 with import module
+def count_element2(lst):
+    """ Count element in the sequence"""
+
+    lst_str = list(map(str, lst))
+    solution = dict(Counter(lst_str))
+    print('#2 Solution:', ', '.join(f"{val} -> {count}" for val, count in solution.items()))
+
+    return solution
 
 
 if __name__ == '__main__':
     count_element([1, 1, 'foo', [1, 2], True, 'foo', 1, [1, 2]])
+
+    # with import module
+    count_element2([1, 1, 'foo', [1, 2], True, 'foo', 1, [1, 2]])
