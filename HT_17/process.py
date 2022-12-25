b85de23473ac9facdb0c1a99e13288679026a886
sergbondckua <sergninja@gmail.com>
2022-12-25 23:52:44 +0200

    add method create_receipt_to_pdf

--- HT_17/process.py
+++ HT_17/process.py
@@ -17,9 +17,9 @@ from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
-
 # Chrome driver manager
 from webdriver_manager.chrome import ChromeDriverManager
+from xhtml2pdf import pisa
 
 # Custom modules
 from reader import Stream  # get stream file csv
@@ -42,18 +42,19 @@ class OrderProcessPlacer:
         :build_robot: Build robot process
         :wait_element: Wait condition element
         :check_alert: Check glitch
-        :link_order_your_robot: Go to page Build and order your robot!
-        :modal_content_button: Closes the modal window
-        :head_select: Select head for Robot
-        :body_input: Select body for Robot
-        :legs_input: Input legs for Robot
-        :shipping_address: Shipping address
-        :button_preview: Press button
-        :button_order: Press button
-        :button_another_order: Press button
+        :go_link_order_robot: Go to page Build and order your robot!
+        :click_button_modal_content: Closes the modal window
+        :select_head: Select head for Robot
+        :input_body: Select body for Robot
+        :input_legs: Input legs for Robot
+        :input_shipping_address: Shipping address
+        :click_button_preview: Press button
+        :click_button_order: Press button
+        :click_button_order_another: Press button
         :get_receipt_id: Get receipt ID
         :source_html_element: Get source HTML element
         :make_screenshot: Make Robot screenshot
+        :create_receipt_to_pdf: Creating Receipt to PDF
         :_clear_folder: Clear all files in folder
     """
     _BASE_DIR = Path(__file__).resolve().parent
@@ -62,7 +63,6 @@ class OrderProcessPlacer:
     orders = Stream().get_dict_csv
     option_arguments = [
         "start-maximized",  # Opens Chrome in maximize mode
-        # "--headless",  # Opens Chrome in background
         "--no-sandbox",  # Disable sandbox
         "--disable-blink-features=AutomationControlled",  # To not detected
         "disable-popup-blocking",  # Disables pop-ups displayed on Chrome
@@ -87,26 +87,27 @@ class OrderProcessPlacer:
     def main_process_order(self):
         """Starting a process ordering"""
         self._clear_folder()
-        self.link_order_your_robot()
-        self.modal_content_button()
+        self.go_link_order_robot()
+        self.click_button_modal_content()
         for order in self.orders:
             self.build_robot(order)
-            self.button_preview()
-            self.button_order()
+            self.click_button_preview()
+            self.click_button_order()
             self.make_screenshot()
+            self.create_receipt_to_pdf()
             self.logging.info(
                 "🟢 #%s has been completed", order["order_number"])
-            self.button_order_another()
-            self.modal_content_button()
+            self.click_button_order_another()
+            self.click_button_modal_content()
         self.logging.info("🏁 All orders is completed")
 
     def build_robot(self, data: dict):
         """input values for build robot"""
         self.wait_element((By.TAG_NAME, "form"))
-        self.head_select(data)
-        self.body_input(data)
-        self.legs_input(data)
-        self.shipping_address(data)
+        self.select_head(data)
+        self.input_body(data)
+        self.input_legs(data)
+        self.input_shipping_address(data)
 
     def wait_element(self, by_element: tuple, timeout: int = 20) -> WebElement:
         """Wait for the element"""
@@ -128,25 +129,25 @@ class OrderProcessPlacer:
         self.logging.warning("A glitch has been detected: 🤬 <%s>", alert.text)
         return True
 
-    def link_order_your_robot(self):
+    def go_link_order_robot(self):
         """Page order bot"""
         self.wait_element((By.LINK_TEXT, "Order your robot!")).click()
         self.logging.info("Go to order page URL: %s", self.browser.current_url)
 
-    def modal_content_button(self):
+    def click_button_modal_content(self):
         """Press button OK"""
         self.wait_element((By.CLASS_NAME, "btn-dark")).click()
 
-    def head_select(self, data: dict):
+    def select_head(self, data: dict):
         """Select a head for the robot"""
         Select(self.browser.find_element(
             By.ID, "head")).select_by_index(data["head"])
 
-    def body_input(self, data: dict):
+    def input_body(self, data: dict):
         """Select a body for the robot"""
         self.browser.find_element(By.ID, f"id-body-{data['body']}").click()
 
-    def legs_input(self, data: dict):
+    def input_legs(self, data: dict):
         """Enter the part number for the legs"""
         legs = self.browser.find_element(
             By.CSS_SELECTOR,
@@ -154,24 +155,24 @@ class OrderProcessPlacer:
         legs.clear()
         legs.send_keys(data["legs"])
 
-    def shipping_address(self, data: dict):
+    def input_shipping_address(self, data: dict):
         """Shipping address"""
         address = self.browser.find_element(By.ID, "address")
         address.clear()
         address.send_keys(data["address"])
 
-    def button_preview(self):
+    def click_button_preview(self):
         """Press button Preview"""
         self.browser.find_element(By.ID, "preview").click()
 
-    def button_order(self):
+    def click_button_order(self):
         """Press button Order"""
         order = self.browser.find_element(By.ID, "order")
         order.click()
         while self.check_alert():
             order.click()
 
-    def button_order_another(self):
+    def click_button_order_another(self):
         """Press button Order Another Robot"""
         order_another = self.wait_element((By.ID, "order-another"))
         order_another.click()
@@ -200,6 +201,20 @@ class OrderProcessPlacer:
         for file in glob.glob(path.join(self._BASE_DIR, "output/*")):
             os.remove(file)
         self.logging.info("The folder has been cleared.")
+
+    def create_receipt_to_pdf(self):
+        """Convert Receipt and image HTML to PDF"""
+        receipt = self.get_receipt_id()
+        image_file = path.join(self._BASE_DIR, f'output/{receipt}_robot.png')
+        pdf_file = path.join(self._BASE_DIR, f'output/{receipt}_robot.pdf')
+        receipt_html = self.source_html_element((By.ID, "receipt"))
+        image_html = f"<center><img src='{image_file}'></center><br>"
+        # open output file
+        with open(pdf_file, "w+b") as output_file:
+            # convert HTML to PDF and write
+            pisa.CreatePDF(image_html + receipt_html, dest=output_file)
+        os.remove(image_file)  # remove temporary screenshot file
+        self.logging.info("📕 Receipt created in PDF")
 
     def __enter__(self):
         self.start_up()  # open browser
